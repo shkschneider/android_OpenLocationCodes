d7245845d7c8c54f71625660f408fdb319d04ad3
shkschneider <shk.schneider@gmail.com>
2015-09-09 15:12:50 +0200

    Cleanup

--- demo/src/main/java/me/shkschneider/openlocationcodes/demo/MainActivity.java
+++ demo/src/main/java/me/shkschneider/openlocationcodes/demo/MainActivity.java
@@ -8,7 +8,6 @@ import android.graphics.Color;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
